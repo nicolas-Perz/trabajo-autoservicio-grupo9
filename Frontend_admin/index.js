97ecc1fd1fb941cf0a6386d661f9e4fca7e4a898
Lea-2005 <lm30.mamaniabc@gmail.com>
2026-06-30 12:57:40 -0300

    Frontend-admin

--- Frontend_admin/index.js
+++ Frontend_admin/index.js
@@ -1,41 +1,46 @@
-import express from "express"
-import connection from "./src/api/database/db.js";
+import express from "express";
+import connection from "./src/api/database/ddbb.js";
 import environments from "./src/api/config/environments.js";
-import cors from "cors";
 
-const PORT = environments.port;
 const app = express();
 
-app.use(cors());
-
-app.use((req, res, next) => {
-    let fecha = new Date();
-    console.log(`[${fecha.toLocaleDateString()} | ${fecha.toLocaleTimeString()}] ${req.method} ${req.url}`)
-    next();
-});
-
 app.get("/", (req, res) => {
-    res.send("Hola mundo!");
+    res.send("!Hola mundo desde LocalHost!");
 });
 
-app.get("/api/libros", async (req,res) => {
-    try{
-        const [rows] = await connection.query("SELECT * FROM libros")
-        res.status(200).json({payload:rows})
-    }catch(e){
-        console.error(e)
+app.listen(3000, () => {
+    console.log(`Servidor corriendo en el puerto 3000`);
+});
+
+// Endpoint para obtener todos los libros.
+app.get("/api/libros", async (req, res) => {
+    try {
+        const [rows] = await connection.query("SELECT * FROM libros");
+
+        res.status(200).json({
+            payload: rows
+        });
+
+    } catch (error) {
+        console.error("Error al obtener libros:", error.message);
     }
 });
 
-app.get("/api/libros/activos", async (req,res) => {
-    try{
-        const [rows] = await connection.query("SELECT * FROM libros WHERE libros.activo = FALSE")
-        res.status(200).json({payload:rows})
-    }catch(e){
-        console.error(e)
+// Endpoint para obtener todos los libros activos.
+app.get("/api/libros/activo", async (req, res) => {
+    try {
+        const [rows] = await connection.query("SELECT * FROM libros WHERE libros.activo = 1");
+
+        res.status(200).json({
+            payload: rows
+        });
+
+    } catch (error) {
+        console.error("Error al obtener libros activos:", error.message);
     }
 });
 
+// Endpoint para obtener todos los libros por ID.
 app.get("/api/libros/:id", async (req, res) => {
     const id = req.params.id;
 
@@ -72,8 +77,4 @@ app.post("/api/libros", async (req, res) => {
             mensaje: "Error interno del servidor."
         });
     }
-});
-
-app.listen(PORT, () => {
-    console.log(`Corriendo en: http://localhost:${PORT}`);
 });
