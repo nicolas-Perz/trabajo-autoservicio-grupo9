70db72c2465106713f4c4b09901b5730c0704c6d
nicolas-Perz <nicolasperez4209@gmail.com>
2026-06-30 12:42:06 -0300

    Frontend basico y endpoint POST sin validaciones

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 import express from "express"
 import connection from "./src/api/database/db.js";
+import cors from "cors"
 
 const app = express()
 const PORT = 3000
+
+// Middlewares
+app.use(express.json()); 
+app.use(cors());
+app.use((req, res, next) => {
+	console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+	next(); // Pasa al siguiente middleware
+});
 
 app.get("/", (req, res) => {
     res.send("Hola mundo!");
@@ -10,7 +19,8 @@ app.get("/", (req, res) => {
 
 app.get("/api/libros", async (req,res) => {
     try{
-        const [rows] = await connection.query("SELECT * FROM libros")
+        let sql = "SELECT * FROM libros"
+        const [rows] = await connection.query(sql)
         res.status(200).json({payload:rows})
     }catch(e){
         console.error(e)
@@ -19,7 +29,8 @@ app.get("/api/libros", async (req,res) => {
 
 app.get("/api/libros/activos", async (req,res) => {
     try{
-        const [rows] = await connection.query("SELECT * FROM libros WHERE libros.activo = FALSE")
+        let sql = "SELECT * FROM libros WHERE libros.activo = FALSE"
+        const [rows] = await connection.query(sql)
         res.status(200).json({payload:rows})
     }catch(e){
         console.error(e)
@@ -28,12 +39,19 @@ app.get("/api/libros/activos", async (req,res) => {
 
 app.get("/api/libros/:id", async (req, res) => {
     const id = req.params.id;
-
-    const [ rows ] = await connection.query("SELECT * FROM libros WHERE libros.id = ?", [id]);
+    let sql = "SELECT * FROM libros WHERE libros.id = ?"
+    const [ rows ] = await connection.query(sql,[id]);
     
     res.status(200).json({
         payload: rows
     });
+});
+
+app.post("/api/libros", async (req, res) => {
+    let {titulo,imagen,genero,precio} = req.body;
+    let sql = "INSERT INTO libros (titulo,imagen,genero,precio) VALUES (?, ?, ?, ?)";
+    await connection.query(sql,[titulo,imagen,genero,precio]);
+    res.status(200).json({message: "Producto creado con exito"});
 });
 
 app.listen(PORT, () => {
