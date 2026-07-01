144922f571cda2038c2896ce91e14efe221f55b5
nicolas-Perz <nicolasperez4209@gmail.com>
2026-06-30 23:03:54 -0300

    Añadidos frontend/get.html y frontend/get.js, eliminada carpeta public/ y modificado <nav> de frontend/index.html

---
+++ frontend/get.js
@@ -0,0 +1,37 @@
+const contenedorlibros = document.querySelector('.contenedor-libros');
+const getLibroForm = document.getElementById('getLibro-form')
+const urlBase = "http://localhost:3000/api/libros"
+
+ getLibroForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    let formData = new FormData(event.target);
+    let data = Object.fromEntries(formData.entries());
+    let libroId = data.libroId;
+    
+    try {
+        let response = await fetch(`${urlBase}/${libroId}`);
+        console.log(response);
+
+        if(response.ok) {
+            let datos = await response.json();
+            console.log(datos);
+            let libro = datos.payload[0];
+            console.log(libro);
+
+                let htmlLibro = `
+                    <li class="li-listados">
+                        <img src="${libro.imagen}" alt="${libro.titulo}" class="img-listados">
+                        <p>Id: ${libro.id} / Nombre: ${libro.titulo} / <strong>Precio: $${libro.precio}</strong></p>
+                    </li>
+                `;
+
+            contenedorlibros.innerHTML = htmlLibro;
+        } else {
+            alert("Error, TO DO agardar a optimizacion")
+        }
+
+    } catch(error) {
+        console.log(error);
+        console.error("Error al obtener el libro: ", error.message);
+    }
+});
