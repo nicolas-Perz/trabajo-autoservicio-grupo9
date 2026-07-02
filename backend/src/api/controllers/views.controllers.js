bbcd7f9924f3b53ef84eb5f14067877ba36d436a
nicolas-Perz <nicolasperez4209@gmail.com>
2026-07-02 11:12:28 -0300

    Implementacion de controladores, rutas y renders via EJS a todas las vistas de backend

--- backend/src/api/controllers/views.controllers.js
+++ backend/src/api/controllers/views.controllers.js
@@ -1,5 +1,5 @@
 import { join,__dirname } from "../utils/index.js"
-import { selectAllLibros } from "../models/libros.models.js" 
+import { insertLibro, selectAllLibros, selectInactiveLibros, selectLibroById, updateLibro } from "../models/libros.models.js" 
 
 export const indexView = async (req,res) => {
     try{
@@ -14,4 +14,47 @@ export const indexView = async (req,res) => {
         console.error(e.message)
         res.status(500).json({message:"Error interno"})
     }
+}
+
+export const indexInactiveView = async (req,res) => {
+    try{
+        const [rows] = await selectInactiveLibros()
+
+        res.render("inactivos",{
+        title: "Inactivos",
+        about: "Libros en estado inactivos",
+        librosInactivosArray: rows
+    })
+    }catch(e){
+        console.error(e.message)
+        res.status(500).json({message:"Error interno"})
+    }
+}
+
+export const getLibroView = (req,res) => {
+    res.render("get",{
+        title: "Consultar libro",
+        about: "Consultar libro via ID:",
+    })
+}
+
+export const putLibrosView = (req,res) => {
+    res.render("put",{
+        title: "Modificar libro",
+        about: "Consultar libro via ID: "
+    })
+}
+
+export const postLibrosView = (req,res) => {
+    res.render("post",{
+        title: "Crear libro",
+        about: "Crear nuevo libro: "
+    })
+}
+
+export const deleteLibrosView = (req,res) => {
+    res.render("delete",{
+        title: "Eliminar libro",
+        about: "Consultar libro via ID: "
+    })
 }
